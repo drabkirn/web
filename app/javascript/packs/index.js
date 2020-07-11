156ff3d15ea6e1fa7d03bb962f7122dd1061ca2e
Aditya Nagla <me@cdadityang.xyz>
2020-07-11 19:25:05 +0530

    V1.1.6: Added airbrake support

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -7,6 +7,7 @@ import PiwikReactRouter from 'piwik-react-router';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { Notifier } from '@airbrake/browser';
 
 import rootReducer from '../store/reducers/rootReducer';
 
@@ -21,27 +22,49 @@ import '../components/custom';
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 if(process.env.NODE_ENV === "production"){
-  // Matomo/Piwik Setup
-  const piwik = PiwikReactRouter({
-    url: 'https://analytics.cdadityang.xyz',
-    siteId: 3
+  // AIRBRAKE START
+  const airbrake = new Notifier({
+    projectId: 281210,
+    projectKey: '01075522b3f0b01a3eca8eda58c948fb'
   });
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const docRoot = document.getElementById('root');
-
-    if(docRoot) {
-      ReactDOM.render(
-        <Provider store={ store }>
-          <Router history={piwik.connectToHistory(history)}>
-            {/* <ScrollToTop /> */}
-            <App />
-          </Router>
-        </Provider>,
-        docRoot
-      );
-    }
+  airbrake.addFilter((notice) => {
+    notice.context.environment = process.env.RAILS_ENV;
+    return notice;
   });
+
+  try {
+    // Matomo/Piwik Setup
+    const piwik = PiwikReactRouter({
+      url: 'https://analytics.cdadityang.xyz',
+      siteId: 3
+    });
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const docRoot = document.getElementById('root');
+
+      if(docRoot) {
+        ReactDOM.render(
+          <Provider store={ store }>
+            <Router history={piwik.connectToHistory(history)}>
+              {/* <ScrollToTop /> */}
+              <App />
+            </Router>
+          </Provider>,
+          docRoot
+        );
+      }
+    });
+  } catch (err) {
+    airbrake.notify(err).then((notice) => {
+      if (notice.id) {
+        console.log('notice id:', notice.id);
+      } else {
+        console.log('notify failed:', notice.error);
+      }
+    });
+  }
+  // AIRBRAKE END
 } else {
   document.addEventListener('DOMContentLoaded', () => {
     const docRoot = document.getElementById('root');
