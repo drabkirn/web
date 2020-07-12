cfffe8e54888eef2461233140ff3df2167575165
Aditya Nagla <me@cdadityang.xyz>
2020-07-12 13:31:54 +0530

    V1.1.8: Remove airbrake from BE and FE

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { Notifier } from '@airbrake/browser';
 
 import rootReducer from '../store/reducers/rootReducer';
 
@@ -20,58 +19,18 @@ import '../components/custom';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-if(process.env.NODE_ENV === "production"){
-  // AIRBRAKE START
-  const airbrake = new Notifier({
-    projectId: 281210,
-    projectKey: '01075522b3f0b01a3eca8eda58c948fb'
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  const docRoot = document.getElementById('root');
 
-  airbrake.addFilter((notice) => {
-    notice.context.environment = process.env.RAILS_ENV;
-    return notice;
-  });
-
-  try {
-    document.addEventListener('DOMContentLoaded', () => {
-      const docRoot = document.getElementById('root');
-
-      if(docRoot) {
-        ReactDOM.render(
-          <Provider store={ store }>
-            <BrowserRouter>
-              {/* <ScrollToTop /> */}
-              <App />
-            </BrowserRouter>
-          </Provider>,
-          docRoot
-        );
-      }
-    });
-  } catch (err) {
-    airbrake.notify(err).then((notice) => {
-      if (notice.id) {
-        console.log('notice id:', notice.id);
-      } else {
-        console.log('notify failed:', notice.error);
-      }
-    });
+  if(docRoot) {
+    ReactDOM.render(
+      <Provider store={ store }>
+        <BrowserRouter>
+          {/* <ScrollToTop /> */}
+          <App />
+        </BrowserRouter>
+      </Provider>,
+      docRoot
+    );
   }
-  // AIRBRAKE END
-} else {
-  document.addEventListener('DOMContentLoaded', () => {
-    const docRoot = document.getElementById('root');
-
-    if(docRoot) {
-      ReactDOM.render(
-        <Provider store={ store }>
-          <BrowserRouter>
-            {/* <ScrollToTop /> */}
-            <App />
-          </BrowserRouter>
-        </Provider>,
-        docRoot
-      );
-    }
-  });
-}
+});
